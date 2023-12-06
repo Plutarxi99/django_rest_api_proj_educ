a9d413c790d2e5f7792586bc0d4e381dc7948eac
Egor Shievanov <shievanov@bk.ru>
2023-12-06 18:41:26 +0300

    исправил метод в views/course

--- education/views/course.py
+++ education/views/course.py
@@ -43,24 +43,16 @@ class CourseViewSet(ModelViewSet):
         """Для отображения одного курса и его состояние подписки, а также уроки"""
         return super().retrieve(self, request, *args, **kwargs)
 
-    # def partial_update(self, request, *args, **kwargs):
-    #     kwargs['partial'] = True
-    #     instance = self.get_object()
-    #     check_update_subs.delay(instance=instance, name_course=instance,
-    #                             list_email_subs=['egor.shievanov@gmail.com'])
-    #     return self.update(request, *args, **kwargs)
     def update(self, request, *args, **kwargs):
         partial = kwargs.pop('partial', False)
         instance = self.get_object()
         serializer = self.get_serializer(instance, data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
-        check_update_subs.delay(instance=Course.objects.get(pk=instance.pk).pk)
-        if getattr(instance, '_prefetched_objects_cache', None):
-            # If 'prefetch_related' has been applied to a queryset, we need to
-            # forcibly invalidate the prefetch cache on the instance.
-            instance._prefetched_objects_cache = {}
 
+        check_update_subs.delay(instance=instance.pk)
+        if getattr(instance, '_prefetched_objects_cache', None):
+            instance._prefetched_objects_cache = {}
         return Response(serializer.data)
 
     def get_queryset(self):
